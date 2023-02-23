7669e52bbd99ee71954f71d0463e97cae0102bc1
DAVIDMIRANDAD <ddmontesm@itc.edu.co>
2023-02-22 23:38:09 -0500

    Registro v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './Components/registro/registro.component';
+import { ClientesComponent } from './Components/clientes/clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule,
